21ba7b7fecf51ebb67afca42373c3cf6c5d78f3e
hverlin <hugues.verlin@gmail.com>
2016-11-26 00:41:57 +0200

    add api endpoints

--- webapp/junction_hate/urls.py
+++ webapp/junction_hate/urls.py
@@ -3,22 +3,60 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework import routers
-from apps.text_api.views import ping, twitter_status, facebook_posts
+from rest_framework.reverse import reverse
 
-router = routers.DefaultRouter()
+from apps.text_api.views import ping, twitter_status, facebook_posts, facebook_comments, facebook_reactions
+
+
+class HybridRouter(routers.DefaultRouter):
+    def __init__(self, *args, **kwargs):
+        super(HybridRouter, self).__init__(*args, **kwargs)
+        self.view_urls = []
+
+    def add_url(self, view):
+        self.view_urls.append(view)
+
+    def get_urls(self):
+        return super(HybridRouter, self).get_urls() + self.view_urls
+
+    def get_api_root_view(self, **kwargs):
+        original_view = super(HybridRouter, self).get_api_root_view()
+
+        def view(request, *args, **kwargs):
+            resp = original_view(request, *args, **kwargs)
+            namespace = request.resolver_match.namespace
+            for view_url in self.view_urls:
+                name = view_url.name
+                url_name = name
+                if namespace:
+                    url_name = namespace + ':' + url_name
+                resp.data[name] = reverse(url_name,
+                                          args=args,
+                                          kwargs=kwargs,
+                                          request=request,
+                                          format=kwargs.get('format', None))
+            return resp
+
+        return view
+
+
+router = HybridRouter(trailing_slash=False)
+
+router.view_urls = [
+    url(r'^ping', ping, name="Ping"),
+    url(r'^twitter_status', twitter_status, name="Twitter status"),
+    url(r'^facebook_comments', facebook_comments, name="facebook_comments"),
+    url(r'^facebook_reactions', facebook_reactions, name="Facebook reactions"),
+    url(r'^facebook_posts', facebook_posts, name="Facebook posts"),
+]
 
 urlpatterns = [
     # utilities
     url(r'^admin/', admin.site.urls),
 
     # API
-    url(r'^', include(router.urls)),
-    url(r'^ping', ping),
-    url(r'^twitter_status', twitter_status),
-    url(r'^facebook_posts', facebook_posts),
-
-    # url(r'^docs/', include('rest_framework_docs.urls')),
-
+    url(r'^api/', include(router.urls)),
+    url(r'^', include('rest_framework_docs.urls')),
 ]
 
 if settings.DEBUG:
