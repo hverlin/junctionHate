5405a9dede4e0502f23c65d8d89d77636387aa99
Drestin <maxime25.chabance@gmail.com>
2016-11-26 00:11:34 +0200

    nltk classifier

---
+++ webapp/apps/classifiers/NltkClassifier.py
@@ -0,0 +1,33 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+class NltkClassifier:
+    """
+    Classify entire strings with nltk's Vader API.
+    """
+
+    def __init__(self):
+        self._analyser = SentimentIntensityAnalyzer()
+
+    def analyse_text(self, txt: str):
+        """
+        Analyse a text.
+        :param txt: a string
+        :return: A dictionary with 4 entries:
+            'compound', 'neg', 'neu', 'pos'
+        """
+        return self._analyser.polarity_scores(txt)
+
+    def is_negative(self, txt: str):
+        """
+        Return True if the text is negative
+        """
+        res = self.analyse_text(txt)
+        return res['compound'] < 0
+
+    def is_positive(self, txt: str):
+        """
+        Return True if the text is positive
+        """
+        res = self.analyse_text(txt)
+        return res['compound'] > 0
