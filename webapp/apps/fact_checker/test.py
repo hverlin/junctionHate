74555dece87d5667e42240a0ca60f2c0f0b00297
Rémy Beulé-Dauzat <remy.beule-dauzat@ensimag.grenoble-inp.fr>
2016-11-26 00:50:15 +0200

    connection search duckduckgo with wotchecker

--- webapp/apps/fact_checker/test.py
+++ webapp/apps/fact_checker/test.py
@@ -1,4 +1,8 @@
 from webapp.apps.fact_checker.DuckduckgoSearch import DuckduckgoSearch
+from wotchecker import WoTChecker
 
 website_liste = DuckduckgoSearch().search_on_html_duckduckgo("donal trump")
-print(len(website_liste))
+print(website_liste)
+wtc = WoTChecker()
+res = wtc.test_websites(*website_liste)
+print(res)
