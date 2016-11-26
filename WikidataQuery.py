62733121b2d8a90856e3815fe8c31116608992fe
Drestin <maxime25.chabance@gmail.com>
2016-11-26 19:29:03 +0200

    Birthdate Wikidata query

--- WikidataQuery.py
+++ WikidataQuery.py
@@ -10,6 +10,27 @@ class WikidataQuery:
     def _extract_qid(url: str) -> str:
         return url.split('/')[-1]
 
+    def _run_query(self, query: str):
+        try:
+            self._sparql.setQuery(query)
+            return self._sparql.query().convert()['results']['bindings']
+        except:
+            return None
+
+    @staticmethod
+    def _get_string(attribute, row):
+        return row[attribute]['value']
+
+    @staticmethod
+    def _get_qid(attribute, row):
+        return WikidataQuery._extract_qid(row[attribute]['value'])
+
+    @staticmethod
+    def _get_date(attribute, row):
+        raw_date = WikidataQuery._get_string(attribute, row)
+
+        return raw_date[:10]
+
     def search_politician(self, firstname: str, lastname: str) -> (str, str):
         """
         Search for a politician
@@ -28,14 +49,24 @@ class WikidataQuery:
             }}
             LIMIT 10
             """.format(firstname, lastname)
-        self._sparql.setQuery(query)
-        results = self._sparql.query().convert()
 
-        politicians = results['results']['bindings']
+        politicians = self._run_query(query)
 
-        if len(politicians) > 0:
-            return politicians[0]['politicianLabel']['value'], politicians[0]['politician']['value']
-
-        else:
+        if not politicians:
             return None
 
+        return self._get_string('politicianLabel', politicians[0]),\
+            self._get_qid('politician', politicians[0])
+
+    def birthdate(self, qid:str) -> str:
+        query = """
+            SELECT ?date WHERE {{
+                wd:{0} wdt:P569 ?date.
+            }}
+        """.format(qid)
+        results = self._run_query(query)
+
+        if not results:
+            return None
+
+        return self._get_date('date', results[0])
