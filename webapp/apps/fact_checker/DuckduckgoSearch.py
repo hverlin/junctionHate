4c755b4536a6ff923b5ea7d133782b7fcf87c48a
Rémy Beulé-Dauzat <remy.beule-dauzat@ensimag.grenoble-inp.fr>
2016-11-27 07:14:17 +0200

    refcator duckduckgo search to easier the next steps

--- webapp/apps/fact_checker/DuckduckgoSearch.py
+++ webapp/apps/fact_checker/DuckduckgoSearch.py
@@ -3,25 +3,33 @@ import re
 from bs4 import BeautifulSoup
 
 
-def search_on_html_duckduckgo(search):
-    refactor_search = search.replace (" ", "+")
+class DuckduckgoSearch:
+    def __init__(self, search):
+        self.search = search.replace(" ", "+")
+        self.request = "https://duckduckgo.com/html/?q=" + self.search
+        result = requests.get(self.request)
+        self.parsed_html = BeautifulSoup(result.text, 'html.parser')
 
-    request = "https://duckduckgo.com/html/?q="+refactor_search
-    result = requests.get(request)
-    parsed_html = BeautifulSoup(result.text, 'html.parser')
-    link_list = parsed_html.body.find_all('a', attrs={'class': 'result__url'})
+    def get_link_list(self):
+        link_list = self.parsed_html.body.find_all('a', attrs={'class': 'result__url'})
 
-    final_website_list = []
-    for link in link_list:
-        if link.get('href'):
-            final_website_list.append(link.get('href'))
+        final_website_list = []
+        for link in link_list:
+            if link.get('href'):
+                final_website_list.append(link.get('href'))
 
-    title_list = parsed_html.body.find_all('a', attrs={'class': 'result__a'})
-    click_bait = 0
-    for a in title_list:
-        res = re.search("\d+ ", a.text)
-        if res:
-            click_bait += 1
+        return final_website_list
+        # return final_website_list,request,click_bait
 
+    def get_request(self):
+        return self.request
 
-    return final_website_list,request,click_bait
+    def estimate_number_click_bait(self):
+        title_list = self.parsed_html.body.find_all('a', attrs={'class': 'result__a'})
+        click_bait = 0
+        for a in title_list:
+            res = re.search(" \d?\d?\d ", a.text)
+            if res:
+                click_bait += 1
+
+        return click_bait
